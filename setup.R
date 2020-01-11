8f3bc6b190e45488a6772d0fee45346fb4047907
Z-ingdotnet <zzho044@aucklanduni.ac.nz>
2020-01-11 12:15:26 +0800

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,4 +1,6 @@
 
+install.packages("cloudml")
+
 library(cloudml）
 gcloud_install()
 
@@ -7,14 +9,17 @@ install_keras()
 
 library(tensorflow)
 install_tensorflow()
-
-remove.packages("tensorflow")
+tf$constant("Hellow Tensorflow")
+#remove.packages("tensorflow")
+x <- matrix(2, ncol = 1, nrow = 1)
+m <- tf$matmul(x, x)
+m
 
 library(tfautograph)
 library(tfdatasets)
 
+#remove.packages("keras")
 
-tf$constant("Hellow Tensorflow")
 
 library(reticulate)
 
@@ -29,4 +34,13 @@ library(keras)
 install_keras(method = c("conda"),
               conda = "auto", version = "default", tensorflow = "gpu")
 
-reticulate::py_install(envname = "r-tensorflow", packages = "numpy")
+
+devtools::install_github("rstudio/tensorflow")
+devtools::install_github("rstudio/keras")
+tensorflow::install_tensorflow()
+tensorflow::tf_config()
+
+
+reticulate::py_discover_config()
+reticulate::use_condaenv("r-tensorflow")
+reticulate::py_config()
